4726535fc1cc88c661ddbc03dc9bde6db7831981
oxg558 <oxg558@cs.bham.ac.uk>
2017-07-14 12:29:33 +0100

    Added parsing part of day8

--- AdventOfCode/src/day8/ledDisplay.hs
+++ AdventOfCode/src/day8/ledDisplay.hs
@@ -1,3 +1,5 @@
+import System.IO
+
 -- NOTE: use this in bash, not cmd/powershell
 -- Based on Graham Hutton's Game of Life code
 
@@ -52,3 +54,40 @@ rotate isRow index amount ((c,r):b) =   if isRow
 -- TODO dumb mod error with:
 -- rotate False 1 1 (rotate True 0 4 (rotate False 1 1 (rect 3 2 [])))
 -- for some reason it still returns (2,4) as a point instead of (2,1) even though I defo use mod'
+
+-- PARSING
+
+splitBy            :: Char -> String -> [String]
+splitBy c ""         =  []
+splitBy c s          =  let (l, s') = break (== c) s
+                      in  l : case s' of
+                                []      -> []
+                                (_:s'') -> splitBy c s''
+                                
+strToInt s = read s :: Int
+
+parse command = let wds = splitBy ' ' command in if length wds == 2 then rectparse wds else rotparse (tail wds)
+
+rectparse :: [String] -> [Int]
+rectparse (_:command:[]) = map strToInt (splitBy 'x' command)
+
+rotparse :: [String] -> [Int]
+rotparse (row:index:_:amount:[]) = [(if row=="row" then 1 else 0),strToInt (drop 2 index), strToInt amount]
+
+-- needs to be used with a board
+executeOne coms =  if length coms == 2 
+                then rect (head coms) (head $ tail coms)
+                else rotate (if (coms !! 0)==1 then True else False) (coms !! 1) (coms !! 2)
+				
+executeAll []     last_board = last_board
+executeAll (x:xs) last_board = executeAll xs (executeOne (parse x) last_board)
+
+-- IO
+
+perform file = do  
+    handle <- openFile file ReadMode  
+    contents <- hGetContents handle  
+    let ls = lines contents
+    showcells $ executeAll ls [];
+	putStr "\n"
+    hClose handle
