0c3be3ed0469512cc925e142a724068142d20d97
Oliver Gratton <ojgratton@gmail.com>
2022-12-08 12:28:23 +0000

    day 8 minor tidy

--- AdventOfCode/2022/day08/solution.py
+++ AdventOfCode/2022/day08/solution.py
@@ -1,9 +1,3 @@
-import os
-os.chdir("/Users/oliver.gratton/Documents/challenges/AdventOfCode/2022/day08")
-
-with open("example") as f:
-    grid_ex = list(map(lambda xs: [int(x) for x in xs.strip()], f.readlines()))
-
 with open("input.txt") as f:
     grid_in = list(map(lambda xs: [int(x) for x in xs.strip()], f.readlines()))
 
@@ -15,6 +9,7 @@ def get_crosshair(i, j, grid):
     return up, down, left, right
 
 def is_visible(dirs, target):
+    # TODO replace with `any` one-liner
     vis = False
     for d in dirs:
         if not d:
@@ -26,6 +21,19 @@ def calc_is_visible(i, j, grid):
     dirs = get_crosshair(i, j, grid)
     yield 1 * is_visible(dirs, grid[i][j])
 
+def calc_viewing_distance(i, j, grid):
+    up, down, left, right = get_crosshair(i, j, grid)
+    
+    score = 1
+    for d in (up[::-1], down, left[::-1], right):
+        subtotal = 0
+        for tree in d:
+            subtotal += 1
+            if tree >= grid[i][j]:
+                break
+        score *= subtotal
+    yield score
+
 def scan_grid(grid, func):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
@@ -34,32 +42,8 @@ def scan_grid(grid, func):
 def part_1(grid):
     return sum(scan_grid(grid, calc_is_visible))
 
-
-
-
-def calc_viewing_distance(i, j, grid):
-    curr = grid[i][j]
-
-    up, down, left, right = get_crosshair(i, j, grid)
-    up = up[::-1]
-    left = left[::-1]
-    
-    score = 1
-    for d in (up, down, left, right):
-        subtotal = 0
-        for tree in d:
-            # if tree <= curr:
-            subtotal += 1
-            if tree >= curr:
-                break
-        score *= subtotal
-    yield score
-
 def part_2(grid):
     return max(scan_grid(grid, calc_viewing_distance))
-
-
-
 
 
 print(part_1(grid_in))
