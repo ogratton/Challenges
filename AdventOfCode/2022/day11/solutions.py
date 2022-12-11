e6279599961028960f96e24d04c42734ff8a6b3c
Oliver Gratton <ojgratton@gmail.com>
2022-12-11 10:37:54 +0000

    refactor day 11 -- dataclasses are a bit overkill

--- AdventOfCode/2022/day11/solutions.py
+++ AdventOfCode/2022/day11/solutions.py
@@ -1,40 +1,39 @@
-from dataclasses import dataclass
 from math import prod
 from typing import Callable, Dict, List, NewType
 
 MonkeyId = NewType("MonkeyId", str)
 
-@dataclass
-class Monkey:
-    id: MonkeyId
-    items: List[int]
-    inspect: Callable[[int], int]
-    divis: int
-    next: Callable[[int], MonkeyId]
-    num_inspects: int = 0
-    
 
-    @classmethod
-    def parse(cls, lines) -> 'Monkey':
-        return cls(
-            id=MonkeyId(lines[0][7]),
-            items=[int(x) for x in lines[1][16:].split(",")],
-            divis=int(lines[3][19:]),
-            inspect=lambda old: eval(lines[2][17:]),
-            next=lambda i, d: MonkeyId(lines[4][25:]) if i % d == 0 else MonkeyId(lines[5][26:])
+class Monkey:
+
+    def __init__(self, lines):
+        self.id: MonkeyId = MonkeyId(lines[0][7])
+        self.items: List[int] = [int(x) for x in lines[1][16:].split(",")]
+        self.divis: int = int(lines[3][19:])
+        self.inspect: Callable[[int], int] = lambda old: eval(lines[2][17:])
+        self.next: Callable[[int], MonkeyId] = (
+            lambda i, d: MonkeyId(lines[4][25:])
+            if i % d == 0
+            else MonkeyId(lines[5][26:])
         )
-    
+
+        self.num_inspects: int = 0
+
     def __repr__(self) -> str:
         return f"Monkey {self.id} after {self.num_inspects} inspections: {self.items}"
 
+
 def parse_infile(filename) -> Dict[MonkeyId, Monkey]:
     with open(filename) as f:
-        raw_monkeys = [[x.strip() for x in l.split("\n")] for l in f.read().split("\n\n")]
+        raw_monkeys = (
+            [x.strip() for x in l.split("\n")] for l in f.read().split("\n\n")
+        )
     monkey_lookup = {}
     for rm in raw_monkeys:
-        m = Monkey.parse(rm)
+        m = Monkey(rm)
         monkey_lookup[m.id] = m
     return monkey_lookup
+
 
 def do_round(n, monkey_lookup, valium, verbose=False):
     for mid, monkey in monkey_lookup.items():
@@ -46,7 +45,7 @@ def do_round(n, monkey_lookup, valium, verbose=False):
                 print(f"Monkey {mid} throws item {worry_level} to monkey {throw_to}")
             monkey_lookup[throw_to].items.append(worry_level)
         monkey.items = []
-    
+
     if verbose:
         print(f"After round {n}:")
         print(*monkey_lookup.values(), sep="\n")
@@ -54,18 +53,13 @@ def do_round(n, monkey_lookup, valium, verbose=False):
     elif not n % 100:
         print(f"Round {n}")
 
-def calculate_answer(ml):
-    top_2 = sorted([m.num_inspects for m in ml.values()], reverse=True)[:2]
-    return prod(top_2)
 
-def part_1():
-    ml = parse_infile("input.txt")
+def part_1(ml):
     for i in range(20):
-        do_round(i+1, ml, lambda x: x//3, verbose=True)
-    
-    print(f"Answer is {calculate_answer(ml)}")
+        do_round(i + 1, ml, lambda x: x // 3, verbose=True)
 
-def part_2():
+
+def part_2(ml):
     """
     No longer limiting anxiety. Numbers will grow huuuge quickly
     if we don't do something about it.
@@ -73,11 +67,13 @@ def part_2():
     the divisible test numbers, we keep the numbers in check without
     changing the outcome
     """
-    ml = parse_infile("input.txt")
     prod_div = prod(m.divis for m in ml.values())
     for i in range(10_000):
-        do_round(i+1, ml, lambda x: x % prod_div)
-    
-    print(f"Answer is {calculate_answer(ml)}")
+        do_round(i + 1, ml, lambda x: x % prod_div)
 
-part_2()
+
+monkeh = parse_infile("input.txt")
+part_2(monkeh)
+answer = prod(sorted([m.num_inspects for m in monkeh.values()], reverse=True)[:2])
+print(f"Answer is {answer}")
+
