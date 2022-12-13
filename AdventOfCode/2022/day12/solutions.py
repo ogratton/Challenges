e4f46c9068a49af1ac20a0accdc91eb7f7ae92ef
Oliver Gratton <ojgratton@gmail.com>
2022-12-13 08:24:31 +0000

    add a stolen solution to the docstring

--- AdventOfCode/2022/day12/solutions.py
+++ AdventOfCode/2022/day12/solutions.py
@@ -1,3 +1,24 @@
+"""
+Much more sensible/concise solution using the same library + numpy stolen from reddit:
+
+```
+import numpy as np, networkx as nx
+
+H = np.array([[*x.strip()] for x in open(0)])
+
+S = tuple(*np.argwhere(H=='S')); H[S] = 'a'
+E = tuple(*np.argwhere(H=='E')); H[E] = 'z'
+
+G = nx.grid_2d_graph(*H.shape, create_using=nx.DiGraph)
+G.remove_edges_from([(a,b) for a,b in G.edges if ord(H[b]) > ord(H[a])+1])
+
+p = nx.shortest_path_length(G, target=E)
+print(p[S], min(p[a] for a in p if H[a]=='a'))
+```
+
+In the space it took me to build the graph I could have done an actual BFS/A*
+"""
+
 import networkx
 
 grid = [l.strip() for l in open("input.txt")]
